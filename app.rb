2cf3f6c3f0368efb998c27eca3389896bef453f6
Mark Miranda <mmiranda@doximity.com>
2022-11-24 13:21:37 -0700

    DO THE THING

--- app.rb
+++ app.rb
@@ -7,17 +7,29 @@ require 'sinatra/reloader' if development?
 require 'net/http'
 require 'json'
 
+ALLOWED_SIGNS = %w(aries taurus gemini cancer leo virgo libra scorpio sagittarius capricorn aquarius pisces)
+
 before do
-  content_type 'application/json'
+  content_type :json
 end
 
 get '/' do
   { message: "hey, everything is okay!" }.to_json
 end
 
-get '/horoscope/:sign' do
-  uri = URI("https://any.ge/horoscope/api/?sign=#{params[:sign]}&type=daily&day=today&lang=en")
-  api_call = Net::HTTP.get(uri)
-  { message: JSON.parse(api_call).first["text"] }.to_json
+post '/horoscope' do
+  response =  if ALLOWED_SIGNS.include?(params[:text].downcase)
+    horoscope_endpoint ="https://any.ge/horoscope/api/?sign=#{params[:text]}&type=daily&day=today&lang=en"
+    uri = URI(horoscope_endpoint)
+    json_response(Net::HTTP.get(uri))
+  else
+    "Hey dummy, that's not a proper zodiac sign. Reach out to Mike Cassano if you need help figuring out what they are. He's an astrological expert!"
+  end
+  { text: "#{params[:text].upcase}: #{response}", response_type: "in_channel" }.to_json
 end
 
+def json_response(json)
+  horoscope_text = JSON.parse(json)[0]["text"]
+  re = /<("[^"]*"|'[^']*'|[^'">])*>/
+  horoscope_text.gsub(re, '')
+end
