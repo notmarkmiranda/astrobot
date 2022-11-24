9e77ef17cd3bf70b7b427bbf5215957cfa7d65db
Mark Miranda <mmiranda@doximity.com>
2022-11-23 22:20:25 -0700

    remove hardcoded sign and add param

--- app.rb
+++ app.rb
@@ -15,8 +15,8 @@ get '/' do
   { message: "hey, everything is okay!" }.to_json
 end
 
-get '/horoscope' do
-  uri = URI('https://any.ge/horoscope/api/?sign=cancer&type=daily&day=today&lang=en')
+get '/horoscope/:sign' do
+  uri = URI("https://any.ge/horoscope/api/?sign=#{params[:sign]}&type=daily&day=today&lang=en")
   api_call = Net::HTTP.get(uri)
   { message: JSON.parse(api_call).first["text"] }.to_json
 end
